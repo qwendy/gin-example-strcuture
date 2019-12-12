c0c082a87e5532d486cd62d2cfb9b5bc4f04ee64
皆休 <1542997629@qq.com>
2019-12-12 11:14:50 +0800

    add graceful-shutdown

--- server/server.go
+++ server/server.go
@@ -1,7 +1,13 @@
 package server
 
 import (
+	"context"
 	"gin-example-structure/config"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -10,5 +16,29 @@ func Init() {
 	config := config.GetConfig()
 	r := NewRouter()
 	log.Info("Server Running On port ", config.GetString("server.port"))
-	r.Run(config.GetString("server.port"))
+	srv := &http.Server{
+		Addr:    config.GetString("server.port"),
+		Handler: r,
+	}
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown: ", err)
+	}
+
+	log.Info("Server exiting")
 }
